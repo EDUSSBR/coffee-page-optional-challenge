22e684c5fce4cf6c1bbc02c183fa95cbefe66daa
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-04-08 13:26:14 -0300

    add Home Page

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+  <main>
+    <h1>Great Coffee</h1>
+    <h1>&gt;Great Code /&lt;</h1>
+    <img src="/assets/rocket-coffee.png" alt="Fotos de cafés" />
+  </main>
+    )
+}
+
+export default Home
