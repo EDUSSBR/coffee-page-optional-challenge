a0362b4a751f4ce679a86817b18aa759e2f2e758
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-04-08 13:25:45 -0300

    add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+export default function Header(): JSX.Element{
+    return (
+        <header>
+        <img src="/assets/logo-desktop.svg" alt="Logo" />
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Menu</li>
+            <li>Recompensas</li>
+            <li>Gift Cards</li>
+            <li>Lojas</li>
+          </ul>
+        </nav>
+        <button>PEGAR MEU CAFÉ</button>
+      </header>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import App, { AppProps } from "next/app";
-import Header from "../components/Header";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
-            <Header />
             <Component {...pageProps} />
         </>
 
