1fdeed3397dfd3f3ec2627fb0b21845e3f3561e1
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-04-08 13:26:55 -0300

    add Header Component to pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import App, { AppProps } from "next/app";
+import Header from "../components/Header";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
+            <Header />
             <Component {...pageProps} />
         </>
 
